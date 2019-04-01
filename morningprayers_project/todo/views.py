43ef588cedd255ce1d79abfea553d77a297301dc
Edward Parker <edward@albany.me.uk>
2019-04-01 12:46:35 +0100

    Second

--- morningprayers_project/todo/views.py
+++ morningprayers_project/todo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
+    return HttpResponse("This is the todo Index page.")
