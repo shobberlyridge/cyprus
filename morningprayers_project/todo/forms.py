24df2f6d614e8a30387c53cd268004e071851eb5
Edward Parker <edward@albany.me.uk>
2019-05-14 18:46:09 +0100

    done for the day

--- morningprayers_project/todo/forms.py
+++ morningprayers_project/todo/forms.py
@@ -6,7 +6,7 @@ from django.forms.fields import DateField
 
 class TodoForm(forms.ModelForm):
 	title = forms.CharField(max_length=25, help_text="Enter title of item.")
-	description = forms.CharField(max_length=100, help_text="Description", initial="initial")
+	description = forms.CharField(max_length=100, help_text="Description")
 	#creation_date = forms.DateField(disabled=True, widget = forms.SelectDateWidget, required=False)
 	target_completion_date = forms.DateField(required = False, widget = forms.SelectDateWidget, help_text="Target Completion Date")
 	start_date = forms.DateField(required = False, widget = forms.SelectDateWidget, help_text="Start Date")
